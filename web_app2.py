10eaa7b896d6bd929f2885808c621448acfedfe7
sqlcyi2008 <123438115@qq.com>
2017-09-14 11:08:47 +0800

    web应用

---
+++ web_app2.py
@@ -0,0 +1,43 @@
+# -*- encoding:utf-8 -*-
+
+import tornado.web
+import tornado.websocket
+import tornado.httpserver
+import tornado.ioloop
+
+
+class IndexPageHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        self.write_message(u"你好，主人！")
+
+    def on_message(self, message):
+        self.write_message(u"Your message was: " + message)
+
+    def on_close(self):
+        pass
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r'/', IndexPageHandler),
+            (r'/ws', WebSocketHandler)
+        ]
+
+        settings = {'template_path': '.','static_path':'static','static_url_prefix':'/static/'}
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+if __name__ == '__main__':
+    ws_app = Application()
+    server = tornado.httpserver.HTTPServer(ws_app)
+    server.listen(8080)
+    tornado.ioloop.IOLoop.instance().start()
