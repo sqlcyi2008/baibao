24144a931c57b05bd8f06f0c1b33754b30d46412
sqlcyi2008 <123438115@qq.com>
2017-09-20 13:58:00 +0800

    本地127.0.0.1抓包,整理。

--- rawsocket_local.py
+++ rawsocket_local.py
@@ -1,42 +1,8 @@
 # coding:utf-8
 import socket
-import struct
-from ctypes import *
 import dpkt
 
 # 监听的主机IPhost = "192.168.1.100"
-
-# IP头定义
-class IP(Structure):
-    _fields_ = [
-        ("ihl",             c_ubyte, 4),
-        ("version",         c_ubyte, 4),
-        ("tos",             c_ubyte),
-        ("len",             c_ushort),
-        ("id",              c_ushort),
-        ("offset",          c_ushort),
-        ("ttl",             c_ubyte),
-        ("protocol_num",    c_ubyte),
-        ("sum",             c_ushort),
-        ("src",             c_uint),
-        ("dst",             c_uint),
-    ]
-
-    def __new__(self, socket_buffer=None):
-        return self.from_buffer_copy(socket_buffer)
-
-    def __init__(self, socket_buffer=None):
-        self.protocol_map = {1: "ICMP", 6: "TCP", 17: "UDP"}
-
-        # readable ip address
-        self.src_address = socket.inet_ntoa(struct.pack("<I", self.src))
-        self.dst_address = socket.inet_ntoa(struct.pack("<I", self.dst))
-
-        # type of protocol
-        try:
-            self.protocol = self.protocol_map[self.protocol_num]
-        except:
-            self.protocol = str(self.protocol_num)
 
 socket_protocol = socket.IPPROTO_IP
 
@@ -49,19 +15,11 @@ sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
 try:
     while True:
         raw_buffer = sniffer.recvfrom(65535)[0]
-
         ipp = dpkt.ip.IP(raw_buffer)
-        #ip.unpack(raw_buffer)
-        #ip = '%d.%d.%d.%d' % tuple(map(ord, list(ipp.data.src)))
-        #print("##"+ipp.data.__class__.__name__)
         if ipp.data.__class__.__name__ == 'TCP' and ipp.data.dport == 8080:
             tcp = ipp.data.data.decode()
             if tcp.startswith('GET') or tcp.startswith('POST'):
-                print(tcp)
-
-        #ip_header = IP(raw_buffer[:20])
-
-        #print ("Protocol: %s %s -> %s " % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+                print(tcp.splitlines()[0])
 
 except KeyboardInterrupt:
     pass
